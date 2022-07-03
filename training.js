28d94f00560ba5c05ca9f80d18c5b0772b71d7e3
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-07-03 11:59:11 +0300

    add

---
+++ training.js
@@ -0,0 +1,14 @@
+let username;
+console.log(typeof username); // "undefined"
+
+let inputValue = null;
+console.log(typeof inputValue); // "object"
+
+const quantity = 17;
+console.log(typeof quantity); // "number"
+
+const message = 'JavaScript is awesome!';
+console.log(typeof message); // "string"
+
+const isSidebarOpen = false;
+console.log(typeof isSidebarOpen); // "boolean"
